2c58ea0f41112b7cdc82d4436636ecd0c733afb6
Md. Rakibul Islam <mrislammisbah@gmail.com>
2024-05-02 09:52:29 +0600

    index

--- index.php
+++ index.php
@@ -48,6 +48,16 @@ $all_books=  $conn->query($sql);
 </head>
 
 <body>
+
+
+
+
+<?php
+    include 'partials/header.php';
+    include  'partials/mobile-header.php'
+    ?>
+
+
     <!-- Quick view -->
     <div class="modal fade custom-modal" id="quickViewModal" tabindex="-1" aria-labelledby="quickViewModalLabel" aria-hidden="true">
         <div class="modal-dialog">
@@ -104,75 +114,30 @@ $all_books=  $conn->query($sql);
                                 </ul>
                             </div>
                         </div>
-                        <div class="col-md-6 col-sm-12 col-xs-12">
-                            <div class="detail-info">
-                                <h3 class="title-detail mt-30">Colorful Pattern Shirts HD450</h3>
-                                <div class="product-detail-rating">
-                                    <div class="pro-details-brand">
-                                        <span> Brands: <a href="shop-grid-right.php">Bootstrap</a></span>
-                                    </div>
-                                    <div class="product-rate-cover text-end">
-                                        <div class="product-rate d-inline-block">
-                                            <div class="product-rating" style="width:90%">
+                         <div class="container">
+                            <div class="row">
+
+                                <div class="col-md-6 col-sm-12 col-xs-12">
+                                    <div class="detail-info">
+                                        <h3 class="title-detail mt-30"><?php echo $row["title"]; ?></h3>
+                                        <div class="bt-1 border-color-1 mt-15 mb-15"></div>
+                                        <div class="short-desc mb-30">
+                                            <p class="font-sm"><?php echo $row["description"]; ?></p>
+                                        </div>
+                                        <!-- Assuming you have other details to display -->
+                                        <div class="bt-1 border-color-1 mt-30 mb-30"></div>
+                                        <div class="detail-extralink">
+                                            <div class="product-extra-link2">
+                                                <button type="submit" class="button button-add-to-cart">Request for this book</button>
+                                                <a aria-label="Add To Wishlist" class="action-btn hover-up" href="shop-wishlist.php"><i class="fi-rs-heart"></i></a>
+                                                <a aria-label="Compare" class="action-btn hover-up" href="shop-compare.php"><i class="fi-rs-shuffle"></i></a>
                                             </div>
                                         </div>
-                                        <span class="font-small ml-5 text-muted"> (25 reviews)</span>
                                     </div>
-                                </div>
-                                <div class="clearfix product-price-cover">
-                                    <div class="product-price primary-color float-left">
-                                        <ins><span class="text-brand">$120.00</span></ins>
-                                        <ins><span class="old-price font-md ml-15">$200.00</span></ins>
-                                        <span class="save-price  font-md color3 ml-15">25% Off</span>
-                                    </div>
-                                </div>
-                                <div class="bt-1 border-color-1 mt-15 mb-15"></div>
-                                <div class="short-desc mb-30">
-                                    <p class="font-sm">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aliquam rem officia, corrupti reiciendis minima nisi modi,!</p>
-                                </div>
 
-                                <div class="attr-detail attr-color mb-15">
-                                    <strong class="mr-10">Color</strong>
-                                    <ul class="list-filter color-filter">
-                                        <li><a href="#" data-color="Red"><span class="product-color-red"></span></a></li>
-                                        <li><a href="#" data-color="Yellow"><span class="product-color-yellow"></span></a></li>
-                                        <li class="active"><a href="#" data-color="White"><span class="product-color-white"></span></a></li>
-                                        <li><a href="#" data-color="Orange"><span class="product-color-orange"></span></a></li>
-                                        <li><a href="#" data-color="Cyan"><span class="product-color-cyan"></span></a></li>
-                                        <li><a href="#" data-color="Green"><span class="product-color-green"></span></a></li>
-                                        <li><a href="#" data-color="Purple"><span class="product-color-purple"></span></a></li>
-                                    </ul>
+                                    <!-- Detail Info -->
                                 </div>
-                                <div class="attr-detail attr-size">
-                                    <strong class="mr-10">Size</strong>
-                                    <ul class="list-filter size-filter font-small">
-                                        <li><a href="#">S</a></li>
-                                        <li class="active"><a href="#">M</a></li>
-                                        <li><a href="#">L</a></li>
-                                        <li><a href="#">XL</a></li>
-                                        <li><a href="#">XXL</a></li>
-                                    </ul>
-                                </div>
-                                <div class="bt-1 border-color-1 mt-30 mb-30"></div>
-                                <div class="detail-extralink">
-                                    <div class="detail-qty border radius">
-                                        <a href="#" class="qty-down"><i class="fi-rs-angle-small-down"></i></a>
-                                        <span class="qty-val">1</span>
-                                        <a href="#" class="qty-up"><i class="fi-rs-angle-small-up"></i></a>
-                                    </div>
-                                    <div class="product-extra-link2">
-                                        <button type="submit" class="button button-add-to-cart">Add to cart</button>
-                                        <a aria-label="Add To Wishlist" class="action-btn hover-up" href="shop-wishlist.php"><i class="fi-rs-heart"></i></a>
-                                        <a aria-label="Compare" class="action-btn hover-up" href="shop-compare.php"><i class="fi-rs-shuffle"></i></a>
-                                    </div>
-                                </div>
-                                <ul class="product-meta font-xs color-grey mt-50">
-                                    <li class="mb-5">SKU: <a href="#">FWM15VKT</a></li>
-                                    <li class="mb-5">Tags: <a href="#" rel="tag">Cloth</a>, <a href="#" rel="tag">Women</a>, <a href="#" rel="tag">Dress</a> </li>
-                                    <li>Availability:<span class="in-stock text-success ml-5">8 Items In Stock</span></li>
-                                </ul>
                             </div>
-                            <!-- Detail Info -->
                         </div>
                     </div>
                 </div>
@@ -180,10 +145,7 @@ $all_books=  $conn->query($sql);
         </div>
     </div>
 
-    <?php
-    include 'partials/header.php';
-    include  'partials/mobile-header.php'
-    ?>
+  
     <main class="main">
 
 
@@ -296,14 +258,26 @@ $all_books=  $conn->query($sql);
                 <div class="tab-content wow fadeIn animated" id="myTabContent">
     <div class="tab-pane fade show active" id="tab-one" role="tabpanel" aria-labelledby="tab-one">
         <div class="row product-grid-4">
-            <?php while($row = mysqli_fetch_assoc($all_books)) { ?>
+        <?php while($row = $all_books->fetch_assoc()) { 
+            
+            
+            if($row["availability_status"]== "yes") {
+            
+            ?>
+
+
                 <div class="col-lg-4 col-md-4 col-12 col-sm-6">
+                    
                     <div class="product-cart-wrap mb-30">
                         <div class="product-img-action-wrap">
                             <div class="product-img product-img-zoom">
                                 <a href="shop-product-full.php">
-                                    <img class="default-img" src="assets2/images/pic2.jpg" alt="">
-                                    <img class="hover-img" src="assets2/images/pic2.jpg" alt="">
+                                <?php if(!empty($row["cover_img"]) && file_exists($row["cover_img"])) { ?>
+                                    <img class="default-img" src="<?php echo $row["cover_img"]; ?>" alt="">
+                                <?php } else { ?>
+                                    <img class="default-img" src="assets/imgs/books/default_cover.png" alt="Default Book Cover">
+                                <?php } ?>
+                                <img class="hover-img" src="assets/imgs/books/default_cover.png" alt="">
                                 </a>
                             </div>
                             <div class="product-action-1">
@@ -326,7 +300,7 @@ $all_books=  $conn->query($sql);
                         </div>
                     </div>
                 </div>
-            <?php } ?>
+            <?php } }?>
         </div>
     </div>
 </div>
